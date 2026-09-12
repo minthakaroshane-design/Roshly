34fe0b6ffb7a749b339b0847470825da6fda7c33
minthakaroshane-design <minthakaroshane@gmail.com>
2026-09-12 09:15:47 +0530

    Add sitemap

--- server/server.js
+++ server/server.js
@@ -1568,7 +1568,20 @@ app.get(
         );
     }
 );
+// =========================================================
+// SITEMAP
+// =========================================================
 
+app.get("/sitemap.xml", (req, res) => {
+    res.type("application/xml");
+
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://roshly-production.up.railway.app/</loc>
+    </url>
+</urlset>`);
+});
 // =========================================================
 // FRONTEND
 // =========================================================
