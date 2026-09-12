8a19e0afac7aebd929128fef5772d5acec6e639b
minthakaroshane-design <minthakaroshane@gmail.com>
2026-09-12 09:46:53 +0530

    Allow production frontend

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ app.use(
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://roshly-production.up.railway.app"
 ];
-
 app.use(
     cors({
         origin: function (origin, callback) {
